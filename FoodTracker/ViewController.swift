74836a5e245f264045f6263940f3e59f07112074
Oliver Cripps <ollycripps@gmail.com>
2019-06-15 13:29:33 +0100

    Add .gitignore and initial project files

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Oliver Cripps on 15/06/2019.
+//  Copyright © 2019 Oliver Cripps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
