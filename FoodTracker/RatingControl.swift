74e34976dbdf1a71ca0e3b9b3297efbdce4f1401
Oliver Cripps <ollycripps@gmail.com>
2019-06-15 17:12:28 +0100

    Create rating control file and link to view

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,22 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Oliver Cripps on 15/06/2019.
+//  Copyright © 2019 Oliver Cripps. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+}
